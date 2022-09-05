1b632fedae81ed861601a9b530bbe0354ebbcbd6
Denis Mushits <horo-braven@yandex.ru>
2022-09-05 19:47:56 +0300

    redoc fix v7

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -112,10 +112,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-REDOC_URL = '/redoc/'
-
-REDOC_ROOT = os.path.join(BASE_DIR, 'redoc')
-
 AUTH_USER_MODEL = 'users.User'
 
 SIMPLE_JWT = {
